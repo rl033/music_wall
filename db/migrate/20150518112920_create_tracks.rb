71cd6d975f844f9ce81da106e9897ef19667c4d2
rl033 <raymond_033@hotmail.com>
2015-05-18 19:24:15 +0000

    Added track model and table

---
+++ db/migrate/20150518112920_create_tracks.rb
@@ -0,0 +1,13 @@
+class CreateTracks < ActiveRecord::Migration
+
+  def change
+    create_table :tracks do |t|
+      t.string :title
+      t.string :author
+      t.string :url
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+
+end
